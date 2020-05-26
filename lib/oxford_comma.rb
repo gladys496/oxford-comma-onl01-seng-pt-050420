89effc6541e421fcdc960a8c9451bbfcbbd81726
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-05-26 18:33:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,12 +2,6 @@
 
 def oxford_comma(array)
  
-  if array.length == 2  
-    return "#{array[0]} and #{array[1]}"
-  elsif array.length > 2 
-    last_word = array.pop
-    new_list = array.join(", ")
-    new_list << ", and #{last_word}"
-  end
+ 
  
 end 
